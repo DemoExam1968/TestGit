6bd653f67bdb89276713259b2216c47b7635abd1
Ирина Матысик <irinamatisik1968@gmail.com>
2024-10-15 11:23:30 +0300

    Сложение двух чисел

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, resultAdd;
+            Console.WriteLine("Введите первое число ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число ");
+            b = Convert.ToInt32(Console.ReadLine());
+            resultAdd = a + b;
+            Console.WriteLine("{0} + {1} = {2}",a,b,resultAdd);
+            Console.ReadKey();
+        }
+    }
+}
